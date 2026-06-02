e0f449fa26d8c70e46fd949e3d9411efe26d1f0d
smajrashi <134830754+smajrashi@users.noreply.github.com>
2026-06-02 05:58:57 +0300

    Update RemindersView.kt

--- composeApp/src/commonMain/kotlin/org/example/dementia_tester_app/ui/screens/dashboard/RemindersView.kt
+++ composeApp/src/commonMain/kotlin/org/example/dementia_tester_app/ui/screens/dashboard/RemindersView.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -112,8 +113,8 @@ fun RemindersView() {
                 .height(80.dp)
                 .padding(vertical = 16.dp),
             colors = ButtonDefaults.buttonColors(
-                containerColor = FormColors.green,
-                contentColor = Color.White
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary
             ),
             enabled = true
         ) {
